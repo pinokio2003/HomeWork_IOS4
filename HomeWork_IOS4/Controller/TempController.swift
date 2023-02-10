7f9efee24c7a823fb07fb4c8241d2ea99817fc93
pinokio2003 <pinokio0476@gmail.com>
2023-02-10 12:35:10 +0200

    Main settings

--- HomeWork_IOS4/Controller/TempController.swift
+++ HomeWork_IOS4/Controller/TempController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TempViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +17,3 @@ class ViewController: UIViewController {
 
 
 }
-

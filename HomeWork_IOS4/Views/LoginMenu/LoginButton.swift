91bd53ce761b1ec32f2496f3c7639fd42261d381
pinokio2003 <pinokio0476@gmail.com>
2023-02-15 17:29:29 +0200

    Big patch login menu

--- HomeWork_IOS4/Views/LoginMenu/LoginButton.swift
+++ HomeWork_IOS4/Views/LoginMenu/LoginButton.swift
@@ -7,26 +7,70 @@
 
 import UIKit
 
-class LoginButton {
+class LoginButton: UIButton{
+     
+    var buttonStile = UIButton()
+    static var multiCornerRadius: CGFloat = 60
     
-    let button: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.configuration = .filled()
-        button.backgroundColor = UIColor(red: 255,
-                                         green: 255,
-                                         blue: 240,
-                                         alpha: 1)
-        button.layer.shadowRadius = 5
-        button.layer.shadowOpacity = 3
-        button.layer.shadowColor = UIColor.gray.cgColor
-        button.setTitle("Login", for: .normal)
-        button.layer.cornerRadius = 60
-        button.setTitleColor(.gray, for: .normal)
+    //MARK: - dark shadow
+        let darkShadow: CALayer = {
+            let darkShadow = CALayer()
+            darkShadow.needsDisplayOnBoundsChange = true
+            darkShadow.shadowColor = UIColor.black.cgColor
+            darkShadow.shadowOffset = CGSize(width: 10, height: 10)
+            darkShadow.shadowRadius = 10
+            darkShadow.shadowOpacity = 0.15
+            darkShadow.cornerRadius = 10
+            darkShadow.backgroundColor = UIColor.customBackgroundColor.cgColor
+            return darkShadow
+        }()
+       
+    //MARK: - light shadow
+        let lightShadow: CALayer = {
+            let lightShadow = CALayer()
+            lightShadow.needsDisplayOnBoundsChange = true
+            lightShadow.shadowColor = UIColor.white.cgColor
+            lightShadow.shadowOffset = CGSize(width: -7, height: -7)
+            lightShadow.shadowRadius = 10
+            lightShadow.shadowOpacity = 0.5
+            lightShadow.cornerRadius = 10
+            lightShadow.backgroundColor = UIColor.customBackgroundColor.cgColor
+            return lightShadow
+        }()
+  
+    //MARK: - init
     
-        
-        return button
-    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupBtn()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     
+    func setupBtn(){
+        addSubview(buttonStile)
+        let multiCornerRadius = LoginButton.multiCornerRadius
+
+        buttonStile.setTitle("Start", for: .normal)
+        buttonStile.backgroundColor = .customBackgroundColor
+        buttonStile.frame = self.bounds
+        buttonStile.layer.cornerRadius = multiCornerRadius
+        autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        buttonStile.layer.insertSublayer(darkShadow, at: 0)
+        buttonStile.layer.insertSublayer(lightShadow, at: 0)
+        darkShadow.cornerRadius = multiCornerRadius
+        lightShadow.cornerRadius = multiCornerRadius
+    }
+    
+    
+    //MARK: - layout subview
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        darkShadow.frame = self.bounds
+        lightShadow.frame = self.bounds
+    }
+  
 }
--- HomeWork_IOS4/Views/LoginMenu/LoginTextField.swift
+++ HomeWork_IOS4/Views/LoginMenu/LoginTextField.swift
@@ -8,25 +8,60 @@
 
 import UIKit
 
-class LoginTextField{
+
+class LoginTextField: UITextField {
     
-    let textField: UITextField = {
-        let field = UITextField()
-        field.translatesAutoresizingMaskIntoConstraints = false
-//        field.placeholder = "print notes "
-        field.borderStyle = .roundedRect
-//        field.inputAccessoryView = .none
-        field.keyboardType = .default
-        field.textAlignment = .left
-        //field.delegate = self
-        field.keyboardAppearance = .default
-        field.clearButtonMode = .always
-        field.font = UIFont(name: "Copperplate-Bold" ,size: 20)
-        field.layer.shadowRadius = 5
-        field.layer.shadowOpacity = 3
-        field.layer.shadowColor = UIColor.gray.cgColor
-        
-        return field
+    lazy var innerShadow: CALayer = {
+        let innerShadow = CALayer()
+        layer.addSublayer(innerShadow)
+        return innerShadow
     }()
+    
+   //MARK: - LayoutSubview
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        applyDesign()
+        
+    }
+//MARK: - Inner Shdow setup
+    func applyDesign() {
+        innerShadow.frame = bounds
+        
+        // Shadow path (1pt ring around bounds)
+        let radius = self.frame.size.height
+        let path = UIBezierPath(roundedRect: innerShadow.bounds.insetBy(dx: -4, dy: -4), cornerRadius:radius)
+        let cutout = UIBezierPath(roundedRect: innerShadow.bounds, cornerRadius:radius).reversing()
+        
+        path.append(cutout)
+        innerShadow.shadowPath = path.cgPath
+        innerShadow.masksToBounds = true
+        
+        // Shadow properties
+        innerShadow.shadowColor = UIColor.darkGray.cgColor
+        innerShadow.shadowOffset = CGSize(width: 1, height: 4)
+        innerShadow.shadowOpacity = 1
+        innerShadow.shadowRadius = 4
+        innerShadow.cornerRadius = self.frame.size.height/2
+        
+    }
 }
 
+//class LoginTextField{
+//
+//    let textField: UITextField = {
+//        let field = UITextField()
+//        field.translatesAutoresizingMaskIntoConstraints = false
+//        field.borderStyle = .roundedRect
+//        field.keyboardType = .default
+//        field.textAlignment = .left
+//        field.keyboardAppearance = .default
+//        field.clearButtonMode = .always
+//        field.font = UIFont(name: "Copperplate-Bold" ,size: 20)
+//        field.layer.shadowRadius = 5
+//        field.layer.shadowOpacity = 3
+//        field.layer.shadowColor = UIColor.gray.cgColor
+//
+//        return field
+//    }()
+//}
